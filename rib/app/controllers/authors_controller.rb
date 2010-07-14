ef5eb61fdb3a33d344e82297f6e9d301df13cbcd
ben <twist@anne.site>
2010-07-14 22:45:58 +0200

    started authors controller

--- rib/app/controllers/authors_controller.rb
+++ rib/app/controllers/authors_controller.rb
@@ -1,2 +1,22 @@
 class AuthorsController < BaseController
+
+
+	def index
+		@authors = Authors.all
+	end
+
+	def new
+
+		@author = Authors.new
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def create
+
+	end
 end
