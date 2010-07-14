a4a1597927fca22fbb9cad79612930d937576201
ben <twist@anne.site>
2010-07-14 20:22:19 +0200

    models to plural
    	step 4) renaming the test classes

--- rib/test/unit/books_test.rb
+++ rib/test/unit/books_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class BookTest < ActiveSupport::TestCase
+class BooksTest < ActiveSupport::TestCase
   # Replace this with your real tests.
   test "the truth" do
     assert true
--- rib/test/unit/publishers_test.rb
+++ rib/test/unit/publishers_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class PublisherTest < ActiveSupport::TestCase
+class PublishersTest < ActiveSupport::TestCase
   # Replace this with your real tests.
   test "the truth" do
     assert true
