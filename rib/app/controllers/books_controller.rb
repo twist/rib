124bc89b1f1becba1d9f84d115f013c1df529c27
twist <twist@twistmobile.webit.de>
2010-07-18 13:54:51 +0200

    genre and type integration, show_book

--- rib/app/controllers/books_controller.rb
+++ rib/app/controllers/books_controller.rb
@@ -10,10 +10,17 @@ class BooksController < BaseController
 		@book = Books.new
 		@publishers = Publishers.find (:all, :order => 'name')
 		@authors_list = Authors.select_list
+		@genres_list = Genres.select_list
+		@types_list = Types.select_list
 	end
 
 	def edit
 	end
+
+  def show
+    @book = Books.find(params[:id])
+
+  end
 
 	def update
 	end
@@ -21,12 +28,15 @@ class BooksController < BaseController
 	def create
 		@book = Books.new params[:books]
 		@book.authors << (Authors.find params[:authors][:id])
+		@book.genres << (Genres.find params[:genres][:id])
 		if @book.valid?
 			@book.save!
 		redirect_to :action => :new
 		else
 			@publishers = Publishers.find (:all, :order => 'name')
 			@authors = Authors.find (:all, :order => 'nachname')
+      @authors_list = Authors.select_list
+      @genres_list = Genres.select_list
 			render :new
 		end
 			
