78d21dd16e08307714c5f707bdff9faca016b871
twist <twist@twistmobile.webit.de>
2010-07-18 14:40:07 +0200

    portal
      some statistics
      linkt to portal from menu

--- rib/app/controllers/portal_controller.rb
+++ rib/app/controllers/portal_controller.rb
@@ -3,6 +3,10 @@ class PortalController < BaseController
 	def index
 
 		@books = Books.find (:all, :order => "titel" )
+    @page_count = 0
+    @books.each do |b|
+      @page_count += b.seiten.to_i
+    end
 		@authors = Authors.find (:all, :order => "nachname" )
 		@publishers = Publishers.find (:all, :order => "name" )
 	end
