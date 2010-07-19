4c32f55180310d701528ffd14320915f7717de0f
twist <bellee@webit.de>
2010-07-19 16:56:44 +0200

    intendation

--- rib/app/models/types.rb
+++ rib/app/models/types.rb
@@ -2,15 +2,15 @@ class Types < ActiveRecord::Base
   has_and_belongs_to_many :books, :join_table => 'buecher_rubrik', 
                                    :foreign_key => 'rubrik_id', 
                                    :class_name => 'Books', 
-	                                 :association_foreign_key => 'buch_id'
+	                           :association_foreign_key => 'buch_id'
 
   set_table_name 'rubriken'
-	def self.select_list
-		auth = Types.find(:all, :order => 'name')
-		select_list = []
-		auth.each  { |g|
-			select_list << ["#{g.name}", g.id]
-		}
-		select_list
-	end
+  def self.select_list
+          auth = Types.find(:all, :order => 'name')
+          select_list = []
+          auth.each  { |g|
+                  select_list << ["#{g.name}", g.id]
+          }
+          select_list
+  end
 end
