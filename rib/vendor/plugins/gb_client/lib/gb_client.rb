dea9c37722bc6ac0ddcf93a7e9f506f25d90a0db
twist <twist@twistmobile.webit.de>
2010-07-20 20:52:59 +0200

    droping nokogiri in favor of Hash.from_xml

--- rib/vendor/plugins/gb_client/lib/gb_client.rb
+++ rib/vendor/plugins/gb_client/lib/gb_client.rb
@@ -7,26 +7,15 @@ require 'nokogiri'
 class GbClient
 
   def search_by_isbn(isbn)
+
+    isbn.gsub!(/-/,"")
     client = HTTPClient.new
-    html = client.get "http://books.google.com/books/feeds/volumes?q=9783936937732&prettyprint=true"
-    puts html.body.content
-    data = parse_xml(html.body.content)
-    pp data
+    html = client.get "http://books.google.com/books/feeds/volumes?q=#{isbn}&prettyprint=true"
+    data = Hash.from_xml html.body.content
 
+    data['feed']['entry'] 
 
   end
 
-  protected
-
-  def parse_xml(src)
-    xml = Nokogiri::XML::Reader(src)
-
-    data = {}
-    pp xml.attributes
-    xml.each {|node|
-      data[node.name.to_sym]= node.attributes
-    }
-    data
-  end
 
 end
--- rib/vendor/plugins/gb_client/test/gb_client_test.rb
+++ rib/vendor/plugins/gb_client/test/gb_client_test.rb
@@ -5,6 +5,6 @@ class GbClientTest < ActiveSupport::TestCase
   def test_search_by_isbn
 
     client = GbClient.new
-    client.search_by_isbn("asdas")
+    client.search_by_isbn("978-936937-73-2")
   end
 end
