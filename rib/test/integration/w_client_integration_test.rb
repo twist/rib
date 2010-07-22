5669d37a25240a927d24ddcf471bda487de6cf68
twist <bellee@webit.de>
2010-07-22 17:48:37 +0200

    notinrepo connected

--- rib/test/integration/w_client_integration_test.rb
+++ rib/test/integration/w_client_integration_test.rb
@@ -7,20 +7,16 @@ class WebOfBooksClientIntegration_test < ActionController::IntegrationTest
 
   def setup
 
-    WClient.any_instance.stubs(:access_key).returns(get_ak)
   end
   def test_client_to_object
 
     client = WClient.new
+    pp NotInRepo.data
+    client.access_key = NotInRepo.data
     book = Books.new
 
     book = client.search_by_isbn("9780595197798", book)
     puts book.inspect
   end
 
-  def get_ak
-
-    return File.new("/home/twist/ak","r").gets.gsub(/\W/,"")
-
-  end
 end
